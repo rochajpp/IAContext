45be22cfb760af501c07ffce603f1f04bfd3565a
João Paulo Rocha <joao.rochap03@gmail.com>
2026-06-11 16:47:22 -0300

    :sparkles: feat: Inicia estutura de projeto

---
+++ src/utils/ignoreRules.ts
@@ -0,0 +1,11 @@
+// Arquivos do projeto que são dispensáveis para ao contexto
+export const DEFAULT_IGNORES = [
+    '**/node_modules/**',
+    '**/dist/**',
+    '**/build/**',
+    '**/.git/**',
+    '**/bin/**',
+    '**/obj/**',
+    '**/coverage/**'
+];
+
